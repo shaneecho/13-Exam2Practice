070a0f70f8b8504c8a62188fbe3bfa077bc67c21
yans2 <yans2@rose-hulman.edu>
2019-01-14 19:47:15 -0500

    done

--- src/practice_problem3.py
+++ src/practice_problem3.py
@@ -35,10 +35,10 @@ import rosegraphics as rg
 def main():
     """ Calls the   TEST   functions in this module. """
     # run_test_practice_problem3a()
-    #run_test_practice_problem3b()
+    # run_test_practice_problem3b()
     # run_test_practice_problem3c()
-    run_test_practice_problem3d()
-    # run_test_practice_problem3e()
+    # run_test_practice_problem3d()
+    run_test_practice_problem3e()
 
 
 def is_prime(n):
@@ -360,11 +360,10 @@ def practice_problem3b(sequence):
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   8 minutes.
     ###########################################################################
-    for k in range(len(sequence)):
+    for k in range(len(sequence)-1):
         if sequence[k] == sequence[len(sequence)-1]:
             return True
-        else:
-            return False
+    return False
 
 def run_test_practice_problem3c():
     """ Tests the    practice_problem3c    function. """
@@ -619,7 +618,7 @@ def practice_problem3d(sequence):
     #         return k
     # return -1
     ###########################################################################
-    # TODO: 6. Just ABOVE this _TODO_, you should have implemented
+    # DONE: 6. Just ABOVE this _TODO_, you should have implemented
     #     a solution for the   practice_problem3d   function.
     #     Here, put ANOTHER solution, as follows:
     #
@@ -759,14 +758,17 @@ def practice_problem3e(sequence):
       :type sequence: list(float)    or tuple(float)
     """
     # -------------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   8 minutes.
     ###########################################################################
-
+    sum = 0
+    for k in range(0,len(sequence),2):
+        sum = sum + sequence[k]
+    return sum
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
